6f9a4e7f8361d53c91c2106d9783a25899bd9e87
coderaiser <mnemonic.enemy@gmail.com>
2012-06-12 14:25:57 +0000

    added some shell comands

--- test/test.js
+++ test/test.js
@@ -1,18 +1,24 @@
 var util = require('util'),
-    exec = require('child_process').exec,
-    child;
+    exec = require('child_process').exec;
 
             
 
-exec('curl http://phantomjs.googlecode.com/files/phantomjs-1.5.0-linux-x86-dynamic.tar.gz',execFunctoin);
-exec('tar -zxf phantomjs-1.5.0-linux-x86-dynamic.tar.gz',execFunctoin);
-exec('./phantomjs/bin/phantomjs',execFunctoin);
+exec('curl http://phantomjs.googlecode.com/files/phantomjs-1.5.0-linux-x86-dynamic.tar.gz',makeExecFunctoin(_1));
 
-function execFunctoin(error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    
-    if (error !== null) {
-        console.log('exec error: ' + error);
-    }
+var _1 = function(){exec('tar -zxf phantomjs-1.5.0-linux-x86-dynamic.tar.gz',makeExecFunctoin(_2));}
+var _2 = function(){exec('./phantomjs/bin/phantomjs',makeExecFunctoin(_3));};
+var _3 = function(){exec('ls',makeExecFunctoin());};
+
+function makeExecFunctoin(pFunc){
+    return function(error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+        else if(pFunc && 
+            typeof pFunc==='function')
+                pFunc();
+    };
 }
