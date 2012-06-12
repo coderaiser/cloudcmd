9cbbd3730eb2ded503e24136be359a2d78d16fac
coderaiser <mnemonic.enemy@gmail.com>
2012-06-12 14:05:18 +0000

    starting test.sh

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var util = require('util'),
     exec = require('child_process').exec,
     child;
 
-child = exec('cd test && chmod +x test.sh && test.sh',
+child = exec('chmod +x ./test/test.sh && ./test/test.sh',
   function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
