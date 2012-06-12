c6af7103e4e991bf917a4d3e180d7483c7e13ff6
coderaiser <mnemonic.enemy@gmail.com>
2012-06-12 13:50:33 +0000

    adding test.sh

---
+++ test/test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+curl http://phantomjs.googlecode.com/files/phantomjs-1.5.0-linux-x86-dynamic.tar.gz
+tar -zxf phantomjs-1.5.0-linux-x86-dynamic.tar.gz
+./phantomjs/bin/phantomjs
+node test.js
