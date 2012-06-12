8e0df8016d26dc9836fc2baa20ff083ecc06f3e2
coderaiser <mnemonic.enemy@gmail.com>
2012-06-12 13:43:50 +0000

    trevis testing with sh

--- test/test.js
+++ test/test.js
@@ -1,2 +1,12 @@
-#!/usr/bin/env node
-console.log('all good');
+var util = require('util'),
+    exec = require('child_process').execFile,
+    child;
+
+child = exec('ls',
+  function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+});
