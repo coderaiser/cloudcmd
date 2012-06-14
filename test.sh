be15a75fedcb9004f664f6f71fd6c7d46204e5a0
coderaiser <mnemonic.enemy@gmail.com>
2012-06-14 20:05:16 +0000

    jshint setted up

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 npm install -g jshint
-jshint client.js --config .jshintrc_client
-jshint server.js lib/*.js --config .jshintrc
+jshint *.js lib/*.js --config .jshintrc
 echo 'only warnings. no errors'
