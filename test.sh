aa94f1db14f449b036ef70e468fb7605fd8d3346
coderaiser <rastacoding@gmail.com>
2012-06-26 11:42:58 +0300

    Update master

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 npm i -g jshint
 echo "jshint server.js client.js lib/cloudfunc.js"
-hint --config ./.jshintrc ./server.js ./client.js ./lib/cloudfunc.js
+jhint --config ./.jshintrc ./server.js ./client.js ./lib/cloudfunc.js
