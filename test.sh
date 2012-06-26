d19e0bc18c46c233ece1917806648f9ce8584b5b
coderaiser <rastacoding@gmail.com>
2012-06-26 11:32:37 +0300

    minor changes

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-npm i jshint
+npm i -g jshint
 echo "jshint server.js client.js lib/cloudfunc.js"
-./node_modules/jshint/bin/hint --config ./.jshintrc ./server.js ./client.js ./lib/cloudfunc.js
+hint --config ./.jshintrc ./server.js ./client.js ./lib/cloudfunc.js
