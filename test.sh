98d548e79a9ca9780538db6acbd9189d89dec059
coderaiser <rastacoding@gmail.com>
2012-06-27 18:09:50 +0300

    add one more echo

--- test.sh
+++ test.sh
@@ -2,4 +2,5 @@
 npm i -g jshint
 echo "jshint server.js client.js lib/cloudfunc.js"
 jshint --config ./.jshintrc ./server.js ./client.js 
+echo "jshint ./lib/cloudfunc.js ./lib/server/minify.js ./lib/client/keyBinding.js"
 jshint --config ./.jshintrc ./lib/cloudfunc.js ./lib/server/minify.js ./lib/client/keyBinding.js
