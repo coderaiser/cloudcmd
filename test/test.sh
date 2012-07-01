f11d3a5b3cf7e00d58e65dbd737c88a817da5d3d
coderaiser <rastacoding@gmail.com>
2012-07-01 11:36:14 +0300

    no need install jshint in node v4, so remove compatibility code

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-npm install jshint
+npm i jshint -g
 echo "jshint server.js client.js lib/cloudfunc.js"
-./node_modules/jshint/bin/hint --config ./.jshintrc ./server.js ./client.js 
+jshint --config ./.jshintrc ./server.js ./client.js 
 echo "jshint ./lib/cloudfunc.js ./lib/server/minify.js ./lib/client/keyBinding.js"
 jshint --config ./.jshintrc ./lib/cloudfunc.js ./lib/server/minify.js ./lib/client/keyBinding.js
