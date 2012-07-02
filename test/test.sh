520676961b0da51f2b8db82bace02fda2395d76e
coderaiser <rastacoding@gmail.com>
2012-07-02 14:04:22 +0300

    cloudfoundry deploying added

--- test/test.sh
+++ test/test.sh
@@ -4,3 +4,10 @@ echo "jshint server.js client.js lib/cloudfunc.js"
 jshint --config ./.jshintrc ./server.js ./client.js 
 echo "jshint ./lib/cloudfunc.js ./lib/server/minify.js ./lib/client/keyBinding.js"
 jshint --config ./.jshintrc ./lib/cloudfunc.js ./lib/server/minify.js ./lib/client/keyBinding.js
+#deploy to cloudcmd-demo.cloudfoundry.com
+gem install vmc
+vmc target api.cloudfoundry.com
+vmc target
+echo 'cloudcmd-demo@ukr.net\nGW14dXlm'|vmc login
+vmc info
+vmc push
