b9761cf4110258b28f149bbf922f9825e25372e3
coderaiser <rastacoding@gmail.com>
2012-07-02 14:13:10 +0300

    cloudfoundry deploying removed, because js-error happend

--- test/test.sh
+++ test/test.sh
@@ -4,9 +4,3 @@ echo "jshint server.js client.js lib/cloudfunc.js"
 jshint --config ./.jshintrc ./server.js ./client.js 
 echo "jshint ./lib/cloudfunc.js ./lib/server/minify.js ./lib/client/keyBinding.js"
 jshint --config ./.jshintrc ./lib/cloudfunc.js ./lib/server/minify.js ./lib/client/keyBinding.js
-gem install vmc
-vmc target api.cloudfoundry.com
-vmc target
-echo 'cloudcmd-demo@ukr.net\nGW14dXlm'|vmc login
-vmc info
-vmc push
