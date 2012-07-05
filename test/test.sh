0d6327b05dd466c23bb0204b97e62da624f7618a
coderaiser <mnemonic.enemy@gmail.com>
2012-07-05 04:06:19 -0400

    cleanup

--- test/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-npm i jshint -g
-echo "jshint server.js client.js lib/cloudfunc.js"
-jshint --config ./.jshintrc ./server.js ./client.js 
-echo "jshint ./lib/cloudfunc.js ./lib/server/minify.js ./lib/client/keyBinding.js"
-jshint --config ./.jshintrc ./lib/cloudfunc.js ./lib/server/minify.js ./lib/client/keyBinding.js
