2f4cb455104f1fdf03ac657eaa11b73fc8854212
coderaiser <mnemonic.enemy@gmail.com>
2012-07-06 04:54:05 -0400

    json files added

--- test/test.sh
+++ test/test.sh
@@ -4,3 +4,5 @@ echo "jshint server.js client.js lib/cloudfunc.js"
 jshint --config ./.jshintrc ./server.js ./client.js 
 echo "jshint ./lib/cloudfunc.js ./lib/server/minify.js ./lib/client/keyBinding.js"
 jshint --config ./.jshintrc ./lib/cloudfunc.js ./lib/server/minify.js ./lib/client/keyBinding.js
+echo "jshint ./package.json ./config.json"
+jshint --config ./.jshintrc ./package.json ./config.json
