3170e26a823324581081523f4edaed5334e3cd5c
coderaiser <mnemonic.enemy@gmail.com>
2012-07-07 12:31:17 -0400

    added submodule minify

--- test/test.sh
+++ test/test.sh
@@ -3,7 +3,7 @@ npm i jshint -g
 echo "jshint server.js client.js lib/cloudfunc.js"
 jshint --config ./.jshintrc ./server.js ./client.js 
 echo "jshint ./lib/cloudfunc.js ./lib/server/minify.js ./lib/client/keyBinding.js"
-jshint --config ./.jshintrc ./lib/cloudfunc.js ./lib/server/minify.js ./lib/client/keyBinding.js
+jshint --config ./.jshintrc ./lib/cloudfunc.js ./lib/server/minify/minify.js ./lib/client/keyBinding.js
 echo "jshint ./package.json ./config.json"
 jshint --config ./.jshintrc ./package.json ./config.json
 npm i uglify-js clean-css html-minifier css-b64-images
