cd4d99dcae4c04c607bc44a3c40b829f881124a0
coderaiser <mnemonic.enemy@gmail.com>
2012-07-07 12:37:54 -0400

    changed to readonly repository url

--- test/test.sh
+++ test/test.sh
@@ -2,7 +2,7 @@
 npm i jshint -g
 echo "jshint server.js client.js lib/cloudfunc.js"
 jshint --config ./.jshintrc ./server.js ./client.js 
-echo "jshint ./lib/cloudfunc.js ./lib/server/minify.js ./lib/client/keyBinding.js"
+echo "jshint ./lib/cloudfunc.js ./lib/server/minify/minify.js ./lib/client/keyBinding.js"
 jshint --config ./.jshintrc ./lib/cloudfunc.js ./lib/server/minify/minify.js ./lib/client/keyBinding.js
 echo "jshint ./package.json ./config.json"
 jshint --config ./.jshintrc ./package.json ./config.json
