bcf05d2cfe71be54ecf5bfa7d45d55abb62470e9
coderaiser <mnemonic.enemy@gmail.com>
2012-07-11 10:05:07 -0400

    minor changes

--- server.js
+++ server.js
@@ -312,6 +312,7 @@ CloudServer.start=function()
                      
     CloudServer.IP   = process.env.IP             ||  /* c9           */
                        CloudServer.IP;
+    
     /* if Cloud Server started on jitsu */
     if(!process.env.HOME.indexOf('/opt/haibu')){
         CloudServer.Port = '0.0.0.0';
