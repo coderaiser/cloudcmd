d8abc72473a88ee7358cfbaf32d33eb9bd08a011
coderaiser <mnemonic.enemy@gmail.com>
2012-07-24 08:25:19 -0400

    .jshintrc moved to test/ folder

--- test/test.sh
+++ test/test.sh
@@ -2,11 +2,11 @@
 #linting js files
 npm i jshint -g
 echo "jshint server.js client.js lib/cloudfunc.js"
-jshint --config ./.jshintrc ./server.js ./client.js 
+jshint --config ./test/.jshintrc ./server.js ./client.js 
 echo "jshint ./lib/cloudfunc.js ./lib/server/minify/minify.js ./lib/client/keyBinding.js"
-jshint --config ./.jshintrc ./lib/cloudfunc.js ./lib/server/minify/minify.js ./lib/client/keyBinding.js
+jshint --config ./test/.jshintrc ./lib/cloudfunc.js ./lib/server/minify/minify.js ./lib/client/keyBinding.js
 echo "jshint ./package.json ./config.json"
-jshint --config ./.jshintrc ./package.json ./config.json
+jshint --config ./test/.jshintrc ./package.json ./config.json
 npm i uglify-js clean-css html-minifier css-b64-images
 #linting css files
 npm i recess -g
