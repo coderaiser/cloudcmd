2e95a304a221e34194f9f9eeffd2820186309a4b
coderaiser <mnemonic.enemy@gmail.com>
2012-08-24 04:59:38 -0400

    minor changes

--- server.js
+++ server.js
@@ -388,7 +388,7 @@ CloudServer._controller=function(pReq, pRes)
             /* if file not in one of caches
              * and minimizing setted then minimize it
              */
-            else if(lName !== 'all.min.css' &&
+            else if(lName.indexOf('min') < 0 &&
                 CloudServer.Minify)
                 CloudServer.Minify.optimize(lName, {
                     cache: true,
