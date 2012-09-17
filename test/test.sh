3afff6ac45d7d0e8e99144e137397f30aacbe42f
coderaiser <mnemonic.enemy@gmail.com>
2012-09-17 03:35:10 -0400

    changed path of minify

--- test/test.sh
+++ test/test.sh
@@ -4,7 +4,7 @@ npm i jshint -g
 echo "jshint server.js client.js lib/cloudfunc.js"
 jshint --config ./test/.jshintrc ./server.js ./client.js 
 echo "jshint ./lib/cloudfunc.js ./lib/server/minify/minify.js ./lib/client/keyBinding.js"
-jshint --config ./test/.jshintrc ./lib/cloudfunc.js ./lib/server/minify/minify.js ./lib/client/keyBinding.js
+jshint --config ./test/.jshintrc ./lib/cloudfunc.js ./node_modules/minify/minify.js ./lib/client/keyBinding.js
 echo "jshint ./package.json ./config.json"
 jshint --config ./test/.jshintrc ./package.json ./config.json
 npm i uglify-js clean-css html-minifier css-b64-images
