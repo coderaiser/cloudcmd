316cbf49f9250da05f0fb72a3c839f35c1115e3c
coderaiser <rastacoding@gmail.com>
2012-10-03 16:50:59 +0300

    added comment

--- cloudcmd.bat
+++ cloudcmd.bat
@@ -1,1 +1,9 @@
+:: -------------------------------------
+:: Changing charset back to 866, becouse
+:: all win32 default commands, thet work
+:: thrue cmd.exe showing out in unicode
+:: charset. So Cloud Commander changes
+:: 866 charset to Unicode 65001 sometime
+:: when it's neaded.
+:: -------------------------------------
 node cloudcmd || chcp 866
