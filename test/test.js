1a3ce4576d16f491e3dbc9f2ba65ab2b91dd9e0a
coderaiser <mnemonic.enemy@gmail.com>
2012-11-20 06:11:44 -0500

    fixed tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,12 @@
-var CloudFunc = require('../lib/cloudfunc');
-var assert = require('assert');
-
-try{
-    var lJSON = [{
+(function(){
+    "use strict";
+    
+    var DIR             = process.cwd() + '/',
+        main            = require(DIR + 'lib/server/main'),
+        
+        CloudFunc       = main.cloudfunc,
+        
+        lJSON = [{
             "path": "/etc/X11/",
             "size": "dir"
         }, {
@@ -17,8 +21,10 @@ try{
             "mode": "100755"
         }];
    
-    console.time('CloudFunc.buildFromJSON');
+    start();
     var lResult = CloudFunc.buildFromJSON(lJSON);
+    end();
+    
     var lExpect =
         '<li class=path>'                                                   +
             '<span class="path-icon clear-cache"'                           +
@@ -27,7 +33,7 @@ try{
             '<span class="path-icon refresh-icon" title="refresh (Ctrl+R)">'+
                 '<a href="/fs/no-js/etc/X11"></a></span>'                   +
             '<span>'                                                        +
-                '<a class=links href="/fs/no-js" title=""/"">/</a>'         +
+                '<a class=links href="/fs/no-js" title="/">/</a>'           +
                 '<a class=links href="/fs/no-js/etc" title="/etc">'         +
                     'etc'                                                   +
                 '</a>/X11/'                                                 +
@@ -40,38 +46,53 @@ try{
             '<span class=owner>owner</span>'                                +
             '<span class=mode>mode</span>'                                  +
         '</li>'                                                             +
-        '<li class=current-file>'                                           +
-            '<span class="mini-icon directory"></span>'                     +
-            '<span class=name><a href="/fs/no-js/etc">..</a></span>'        +
-            '<span class=size>&lt;dir&gt;</span>'                           +
-            '<span class=owner>.</span><span class=mode></span>'            +
-        '</li>'                                                             +
-        '<li class>'                                                        +
+        '<li draggable class=current-file>'                                 +
             '<span class="mini-icon directory"></span>'                     +
             '<span class=name>'                                             +
-                '<a href="/fs/no-js/etc/X11/applnk">applnk</a>'             +
+                '<a href="/fs/no-js/etc" draggable=true>..</a>'             +
             '</span>'                                                       +
             '<span class=size>&lt;dir&gt;</span>'                           +
-            '<span class=owner>root</span>'                                 +
-            '<span class=mode>rwx r-x r-x</span>'                           +
+            '<span class=owner>.</span>'                                    +
+            '<span class=mode></span>'                                      +
         '</li>'                                                             +
-        '<li class>'                                                        +
-            '<span class="mini-icon text-file"></span>'                     +
-            '<span class=name>'                                             +
-                '<a href="/fs/no-js/etc/X11/prefdm" target="_blank">'       +
-                    'prefdm'                                                +
+        '<li draggable class>'                                              +
+            '<span draggable class="mini-icon directory"></span>'           +
+            '<span draggable class=name>'                                   +
+                '<a href="/fs/no-js/etc/X11/applnk" '                       +
+                    'title="applnk" draggable=true>applnk</a>'              +
+            '</span>'                                                       +
+            '<span draggable class=size>&lt;dir&gt;</span>'                 +
+            '<span draggable class=owner>root</span>'                       +
+            '<span draggable class=mode>rwx r-x r-x</span>'                 +
+        '</li>'                                                             +
+        '<li draggable class>'                                              +
+            '<span draggable class="mini-icon text-file"></span>'           +
+            '<span draggable class=name>'                                   +
+                '<a href="/fs/no-js/etc/X11/prefdm" '                       +
+                    'target="_blank" title="prefdm" draggable=true>'        +
+                        'prefdm'                                            +
                 '</a>'                                                      +
             '</span>'                                                       +
-            '<span class=size>1.30kb</span>'                                +
-            '<span class=owner>root</span>'                                 +
-            '<span class=mode>rwx r-x r-x</span>'                           +
-        '</li>1';
+            '<span draggable class=size>1.30kb</span>'                      +
+            '<span draggable class=owner>root</span>'                       +
+            '<span draggable class=mode>rwx r-x r-x</span>'                 +
+        '</li>';
+        
+    for(var i = 0, n = lExpect.length; i < n; i++)
+        if(lResult[i] !== lExpect[i]){
+            console.log('Error in char number: ' + i    + '\n' +
+                        'Expect: ' + lExpect.substr(i)  + '\n' +
+                        'Result: ' + lResult.substr(i) );
+            break;
+        }
+    if(i===n)
+        console.log('CloudFunc.buildFromJSON: OK');
     
-        console.timeEnd('CloudFunc.buildFromJSON');
-    assert.equal(
-        lResult,
-        lExpect, 'Something wrong in buildFromJSON');
-}
-catch(pError){
-    console.log(pError);
-}
+    
+    function start(){
+        return console.time('CloudFunc.buildFromJSON');
+    }
+    function end(){
+        return console.timeEnd('CloudFunc.buildFromJSON');
+    }
+})();
