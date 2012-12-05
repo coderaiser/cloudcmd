e56fa1354608d62d0c0179ff37dca5992d4b0226
coderaiser <mnemonic.enemy@gmail.com>
2012-12-05 10:25:33 -0500

    added shell directory with shell files

---
+++ shell/secret.sh
@@ -0,0 +1,13 @@
+#
+# part of Cloud Commander
+# *nix version
+# secrets of github and dropbox
+# must not be shared
+# http://github.com/coderaiser/cloudcmd
+#
+# for using just add $-symbol on start of name
+# like $github_secret
+
+export github_secret=e21b3724d84f0f7570d2d04d8e055f3cc3be3071
+export dropbox_key=0nd3ssnp5fp7tqs
+export dropbox_chooser_key=o7d6llji052vijk
