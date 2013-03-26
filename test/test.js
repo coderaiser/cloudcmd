5960c9523d5523089a124e1959caa3b4b50ce9f5
coderaiser <mnemonic.enemy@gmail.com>
2013-03-26 10:34:03 -0400

    added test file

--- test/test.js
+++ test/test.js
@@ -1,10 +1,19 @@
 (function(){
     'use strict';
     
-    var DIR             = process.cwd() + '/',
-        main            = require(DIR + 'lib/server/main'),
+    var DIR                 = process.cwd() + '/',
+        main                = require(DIR + 'lib/server/main'),
+
+        Util                = main.util,
         
-        CloudFunc       = main.cloudfunc,
+        CloudFunc           = main.cloudfunc,
+        HTMLDIR             = main.HTMLDIR,
+        
+        TEMPLATEPATH        = HTMLDIR + 'file.html',
+        PATHTEMPLATE_PATH   = HTMLDIR + 'path.html',
+        EXPECT_PATH         = DIR + 'test/expect.html',
+        
+        Files               = [TEMPLATEPATH, PATHTEMPLATE_PATH, EXPECT_PATH],
         
         lJSON = [{
             "path": "/etc/X11/",
@@ -19,80 +28,59 @@
             "size": 1328,
             "uid": 0,
             "mode": "100755"
-        }];
-   
-    start();
-    var lResult = CloudFunc.buildFromJSON(lJSON);
-    end();
-    
-    var lExpect =
-        '<li class=path>'                                                   +
-            '<span class="path-icon clear-cache"'                           +
-                'id=clear-cache title="clear cache (Ctrl+D)">'              +
-            '</span>'                                                       +
-            '<span class="path-icon refresh-icon" title="refresh (Ctrl+R)">'+
-                '<a href="/fs/etc/X11"></a></span>'                         +
-            '<span>'                                                        +
-                '<a class=links href="/fs" title="/">/</a>'                 +
-                '<a class=links href="/fs/etc" title="/etc">'               +
-                    'etc'                                                   +
-                '</a>/X11/'                                                 +
-            '</span>'                                                       +
-        '</li>'                                                             +
-            '<li class=fm-header>'                                          +
-            '<span class=mini-icon></span>'                                 +
-            '<span class=name>name</span>'                                  +
-            '<span class=size>size</span>'                                  +
-            '<span class=owner>owner</span>'                                +
-            '<span class=mode>mode</span>'                                  +
-        '</li>'                                                             +
-        '<li draggable class>'                                              +
-            '<span class="mini-icon directory"></span>'                     +
-            '<span class=name>'                                             +
-                '<a href="/fs/etc" draggable=true>..</a>'                   +
-            '</span>'                                                       +
-            '<span class=size>&lt;dir&gt;</span>'                           +
-            '<span class=owner>.</span>'                                    +
-            '<span class=mode></span>'                                      +
-        '</li>'                                                             +
-        '<li draggable class>'                                              +
-            '<span draggable class="mini-icon directory"></span>'           +
-            '<span draggable class=name>'                                   +
-                '<a href="/fs/etc/X11/applnk" '                             +
-                    'title="applnk" draggable=true>applnk</a>'              +
-            '</span>'                                                       +
-            '<span draggable class=size>&lt;dir&gt;</span>'                 +
-            '<span draggable class=owner>root</span>'                       +
-            '<span draggable class=mode>rwx r-x r-x</span>'                 +
-        '</li>'                                                             +
-        '<li draggable class>'                                              +
-            '<span draggable class="mini-icon text-file"></span>'           +
-            '<span draggable class=name>'                                   +
-                '<a href="/fs/etc/X11/prefdm" '                             +
-                    'target="_blank" title="prefdm" draggable=true>'        +
-                        'prefdm'                                            +
-                '</a>'                                                      +
-            '</span>'                                                       +
-            '<span draggable class=size>1.30kb</span>'                      +
-            '<span draggable class=owner>root</span>'                       +
-            '<span draggable class=mode>rwx r-x r-x</span>'                 +
-        '</li>';
+        }],
         
-    for(var i = 0, n = lExpect.length; i < n; i++)
-        if(lResult[i] !== lExpect[i]){
-            console.log('Error in char number: ' + i    + '\n' +
-                        'Expect: ' + lExpect.substr(i)  + '\n' +
-                        'Result: ' + lResult.substr(i) );
-            break;
+        
+        Expect =
+            '<li class=path>'                                                   +
+                '<span class="path-icon clear-cache" '                          +
+                    'id=clear-cache title="clear cache (Ctrl+D)">'              +
+                '</span>'                                                       +
+                '<span class="path-icon refresh-icon" title="refresh (Ctrl+R)">'+
+                    '<a href="/fs/etc/X11"></a></span>'                         +
+                '<span>'                                                        +
+                    '<a class=links href="/fs" title="/">/</a>'                 +
+                    '<a class=links href="/fs/etc" title="/etc">'               +
+                        'etc'                                                   +
+                    '</a>/X11/'                                                 +
+                '</span>'                                                       +
+            '</li>'                                                             +
+            '<li class=fm-header>'                                              +
+                '<span class=mini-icon></span>'                                 +
+                '<span class=name>name</span>'                                  +
+                '<span class=size>size</span>'                                  +
+                '<span class=owner>owner</span>'                                +
+                '<span class=mode>mode</span>'                                  +
+            '</li>';
+   
+    
+    main.readFiles(Files, function(pErrors, pFiles){
+        if(pErrors)
+            Util.log(pErrors);
+        else{
+            console.time('CloudFunc.buildFromJSON');
+            
+            var lTemplate       = pFiles[TEMPLATEPATH].toString(),
+                lPathTemplate   = pFiles[PATHTEMPLATE_PATH].toString(),
+                lExpect         = pFiles[EXPECT_PATH].toString(),
+                
+                lResult         = CloudFunc.buildFromJSON(lJSON, lTemplate, lPathTemplate);
+                
+                Expect          += lExpect;
+                
+                for(var i = 0, n = Expect.length; i < n; i++)
+                    if(lResult[i] !== Expect[i]){
+                        console.log('Error in char number: ' + i    + '\n' +
+                                    'Expect: ' + Expect.substr(i)  + '\n' +
+                                    'Result: ' + lResult.substr(i) );
+                        break;
+                    }
+                
+                    if(i===n)
+                        console.log('CloudFunc.buildFromJSON: OK');    
+            
+            console.timeEnd('CloudFunc.buildFromJSON');
         }
-    if(i===n)
-        console.log('CloudFunc.buildFromJSON: OK');
-    
-    
-    function start(){
-        return console.time('CloudFunc.buildFromJSON');
-    }
-    function end(){
-        return console.timeEnd('CloudFunc.buildFromJSON');
-    }
+    });
+
 })();
