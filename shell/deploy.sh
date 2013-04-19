60ed76b1fd546e1aa6785f73f2873bf117a8871d
coderaiser <mnemonic.enemy@gmail.com>
2013-04-19 09:24:01 -0400

    removed nodester and heroku

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -7,10 +7,5 @@ echo 'appfog'
 af update
 echo '://cloudcmd.aws.af.cm'
 echo 'cloud foundry'
-vmc update
+vmc push
 echo '://cloudcmd.cloudfoundry.com'
-echo 'nodester'
-git push nodester master
-echo 'heroku'
-git push heroku master
-cd shell
