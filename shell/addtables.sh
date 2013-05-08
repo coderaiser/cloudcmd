a2b8407dd64baa7dbc00ca2a6464f4feccf0e7ba
coderaiser <mnemonic.enemy@gmail.com>
2013-05-08 12:14:37 -0400

    fixed typo in README

--- shell/addtables.sh
+++ shell/addtables.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-su iptables -t nat -L # look rules before
-su iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8000
-su iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-ports 4430
-su iptables -t nat -L # look reles after
+sudo iptables -t nat -L # look rules before
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8000
+sudo iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-ports 4430
+sudo iptables -t nat -L # look reles after
 
-#su iptables -t nat -D PREROUTING 1
-#su iptables -t nat -D PREROUTING 2
+#sudo iptables -t nat -D PREROUTING 1
+#sudo iptables -t nat -D PREROUTING 2
