86842db18659f7e98baf376e5c5aab2ccaf0632b
coderaiser <mnemonic.enemy@gmail.com>
2013-08-06 14:10:22 +0000

    feature(shell) add log

---
+++ shell/log.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+if test -z $1
+    then
+        echo "log.sh <tag>"
+    else
+        git log $1..HEAD --pretty=format:"* %s" --grep feature
+        git log $1..HEAD --pretty=format:"* %s" --grep fix
+        git log $1..HEAD --pretty=format:"* %s" --grep refactor
+fi
