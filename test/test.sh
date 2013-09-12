d41f898464740af4b393800a8a275dbf7cfeb3a3
coderaiser <mnemonic.enemy@gmail.com>
2013-09-12 15:24:32 +0300

    refactor(jshint) ./json/config.json, ./json/modules.json -> ./json/*.json

--- test/test.sh
+++ test/test.sh
@@ -2,8 +2,8 @@
 npm i recess jshint
 echo "jshint lib/*.js lib/client/*.js lib/client/storage/*.js lib/server/*.js"
 node_modules/jshint/bin/jshint --config test/.jshintrc lib/*.js lib/client/*.js lib/client/storage/*.js lib/server/*.js
-echo "jshint ./package.json ./json/config.json ./json/modules.json"
-node_modules/jshint/bin/jshint --config test/.jshintrc ./package.json ./json/config.json ./json/modules.json
+echo "jshint package.json json/*.json"
+node_modules/jshint/bin/jshint --config test/.jshintrc package.json json/*.json
 #linting css files
 echo "recess css/*.css"
 ./node_modules/recess/bin/recess css/*.css
