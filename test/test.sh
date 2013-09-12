e6d83be2b6587ab98c6d656f55379a50e1a4aaae
coderaiser <mnemonic.enemy@gmail.com>
2013-09-12 15:22:05 +0300

    refactor(jshint) add lib/*.js, lib/client/*.js, lib/client/storage/*.js

--- test/test.sh
+++ test/test.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 npm i recess jshint
-echo "jshint lib: server.js, client.js, cloudcmd.js, server/*.js"
-node_modules/jshint/bin/jshint --config test/.jshintrc lib/server.js lib/client.js cloudcmd.js lib/server/*.js
-echo "jshint lib/cloudfunc.js lib/client/key.js"
-node_modules/jshint/bin/jshint --config test/.jshintrc lib/util.js lib/cloudfunc.js lib/client/key.js
-echo "jshint lib/client: dom.js, polyfill.js, menu.js, socket.js, console.js, view.js storage/_github.js, edit.js"
-node_modules/jshint/bin/jshint --config test/.jshintrc lib/client/dom.js lib/client/polyfill.js lib/client/menu.js lib/client/socket.js ./lib/client/console.js lib/client/view.js lib/client/storage/_github.js lib/client/edit.js
+echo "jshint lib/*.js lib/client/*.js lib/client/storage/*.js lib/server/*.js"
+node_modules/jshint/bin/jshint --config test/.jshintrc lib/*.js lib/client/*.js lib/client/storage/*.js lib/server/*.js
 echo "jshint ./package.json ./json/config.json ./json/modules.json"
 node_modules/jshint/bin/jshint --config test/.jshintrc ./package.json ./json/config.json ./json/modules.json
 #linting css files
