c6069dffbeb8b26d83ad6bc84cb3742c9d208a70
coderaiser <mnemonic.enemy@gmail.com>
2013-09-12 14:51:52 +0300

    feature(jshint) add lib/server/*.js, json/modules.json, change echo

--- test/test.sh
+++ test/test.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 npm i recess jshint
-echo "jshint server.js client.js cloudcmd.js"
-node_modules/jshint/bin/jshint --config test/.jshintrc lib/server.js lib/client.js cloudcmd.js
+echo "jshint lib: server.js, client.js, cloudcmd.js, server/*.js"
+node_modules/jshint/bin/jshint --config test/.jshintrc lib/server.js lib/client.js cloudcmd.js lib/server/*.js
 echo "jshint lib/cloudfunc.js lib/client/key.js"
 node_modules/jshint/bin/jshint --config test/.jshintrc lib/util.js lib/cloudfunc.js lib/client/key.js
-echo "lib/client/dom.js lib/client/polyfill.js lib/client/menu.js lib/client/socket.js ./lib/client/console.js lib/client/view.js lib/client/storage/_github.js lib/client/menu.js lib/client/edit.js"
-node_modules/jshint/bin/jshint --config test/.jshintrc lib/client/dom.js lib/client/polyfill.js lib/client/menu.js lib/client/socket.js ./lib/client/console.js lib/client/view.js lib/client/storage/_github.js lib/client/menu.js lib/client/edit.js
-echo "jshint ./package.json ./json/config.json"
-node_modules/jshint/bin/jshint --config test/.jshintrc ./package.json ./json/config.json
+echo "jshint lib/client: dom.js, polyfill.js, menu.js, socket.js, console.js, view.js storage/_github.js, edit.js"
+node_modules/jshint/bin/jshint --config test/.jshintrc lib/client/dom.js lib/client/polyfill.js lib/client/menu.js lib/client/socket.js ./lib/client/console.js lib/client/view.js lib/client/storage/_github.js lib/client/edit.js
+echo "jshint ./package.json ./json/config.json ./json/modules.json"
+node_modules/jshint/bin/jshint --config test/.jshintrc ./package.json ./json/config.json ./json/modules.json
 #linting css files
 echo "recess css/*.css"
 ./node_modules/recess/bin/recess css/*.css
