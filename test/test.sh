f6cce3f70678611772c82cccdb199a1ad113d2f7
coderaiser <mnemonic.enemy@gmail.com>
2013-09-12 14:46:00 +0300

    chore(test) echo: keyBinding -> key, ie -> polyfill, viewer -> view, editor/_… …codemirror -> edit, terminal -> console

--- test/test.sh
+++ test/test.sh
@@ -2,9 +2,9 @@
 npm i recess jshint
 echo "jshint server.js client.js cloudcmd.js"
 node_modules/jshint/bin/jshint --config test/.jshintrc lib/server.js lib/client.js cloudcmd.js
-echo "jshint lib/cloudfunc.js lib/client/keyBinding.js"
+echo "jshint lib/cloudfunc.js lib/client/key.js"
 node_modules/jshint/bin/jshint --config test/.jshintrc lib/util.js lib/cloudfunc.js lib/client/key.js
-echo "lib/client/dom.js lib/client/ie.js lib/client/menu.js lib/client/socket.js ./lib/client/terminal.js lib/client/viewer.js lib/client/storage/_github.js lib/client/menu.js lib/client/editor/_codemirror.js"
+echo "lib/client/dom.js lib/client/polyfill.js lib/client/menu.js lib/client/socket.js ./lib/client/console.js lib/client/view.js lib/client/storage/_github.js lib/client/menu.js lib/client/edit.js"
 node_modules/jshint/bin/jshint --config test/.jshintrc lib/client/dom.js lib/client/polyfill.js lib/client/menu.js lib/client/socket.js ./lib/client/console.js lib/client/view.js lib/client/storage/_github.js lib/client/menu.js lib/client/edit.js
 echo "jshint ./package.json ./json/config.json"
 node_modules/jshint/bin/jshint --config test/.jshintrc ./package.json ./json/config.json
