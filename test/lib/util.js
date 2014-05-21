18e1f4ce22238473a5c9613a16ec6844812530d7
coderaiser <mnemonic.enemy@gmail.com>
2014-05-21 05:19:25 -0400

    feature(util) add tests

---
+++ test/lib/util.js
@@ -0,0 +1,64 @@
+(function() {
+    'use strict';
+    
+    /*global describe, it */
+    
+    var should  = require('should'),
+        DIR     = '../../',
+        Util    = require(DIR + 'lib/util');
+    
+    describe('Util', function() {
+        describe('getExt', function() {
+            it('should return "" when extension is none', function() {
+                var EXT     = '',
+                    name    = 'file-withot-extension',
+                    ext     = Util.getExt(name);
+                
+                 should(ext).eql(EXT);
+            });
+            
+            it('should return ".png" in files "picture.png"', function() {
+                var EXT     = '.png',
+                    name    = 'picture.png',
+                    ext     = Util.getExt(name);
+                
+                 should(ext).eql(EXT);
+            });
+        });
+        
+        describe('checkExt', function() {
+            it('should return true when extension is same', function() {
+                var EXT     = 'png',
+                    name    = 'picture.png',
+                    same    = Util.checkExt(name, EXT);
+                
+                 same.should.be.true;
+            });
+            
+            it('should return false when extension is not same', function() {
+                var EXT     = 'jpg',
+                    name    = 'picture.png',
+                    same    = Util.checkExt(name, EXT);
+                
+                 same.should.be.false;
+            });
+            
+            it('should return true when one item of extensions array is same', function() {
+                var EXT     = ['jpg', 'png'],
+                    name    = 'picture.png',
+                    same    = Util.checkExt(name, EXT);
+                
+                 same.should.be.true;
+            });
+            
+            it('should return false when no one item of extensions array is same', function() {
+                var EXT     = ['jpg', 'gif'],
+                    name    = 'picture.png',
+                    same    = Util.checkExt(name, EXT);
+                
+                 same.should.be.false;
+            });
+        });
+    });
+    
+})();
