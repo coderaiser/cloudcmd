ffe2656b7774b74cf2918d0ae3e5f6a5af18e301
coderaiser <mnemonic.enemy@gmail.com>
2014-07-10 10:44:56 -0400

    refactor(cloudcmd) argvFirst -> argvLast

--- bin/cloudcmd.js
+++ bin/cloudcmd.js
@@ -8,8 +8,9 @@
         Util        = require(DIR + 'util'),
         argv        = process.argv,
         length      = argv.length - 1,
-        argvFirst   = argv[length],
-        isTest      = Util.isContainStr(argvFirst, 'test');
+        argvLast    = argv[length],
+        regExp      = new RegExp('^test'),
+        isTest      = argvLast.match(regExp);
     
     if (isTest) {
         Util.log('Cloud Commander testing mode');
