1202c8b68ad9e174733681525446d53ed56ea75c
coderaiser <mnemonic.enemy@gmail.com>
2014-07-11 02:30:16 -0400

    feature(cloudcmd) add -v, --version

--- bin/cloudcmd.js
+++ bin/cloudcmd.js
@@ -3,25 +3,45 @@
 (function(){
     'use strict';
     
-    var CloudCmd    = require('../cloudcmd'),
+    var Info        = require('../package'),
+        
         DIR         = '../lib/',
         Util        = require(DIR + 'util'),
         argv        = process.argv,
         length      = argv.length - 1,
-        argvLast    = argv[length],
-        isTest;
+        argvLast    = argv[length];
     
     switch (argvLast) {
+    default:
+        start({});
+        break;
+    
     case '--test':
         Util.log('Cloud Commander testing mode');
         Util.log('argv: ', argv);
         
-        isTest = true;
+        start({
+            isTest: true
+        });
+        break;
+    
+    case '-v':
+        version();
+        break;
+    
+    case '--version':
+        version();
         break;
     }
     
-    CloudCmd.start({
-        isTest: isTest
-    });
+    function version() {
+        console.log('v' + Info.version);
+    }
+    
+    function start(params) {
+        var cloudcmd    = require('../cloudcmd');
+        
+        cloudcmd.start(params);
+    }
     
 })();
