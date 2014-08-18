31ae0acea69fef71e7ba51b7b81e2bdb0c4582f3
coderaiser <mnemonic.enemy@gmail.com>
2014-08-18 07:50:18 -0400

    feature(log) if no features or not fixes do not add to changelog

--- shell/log.sh
+++ shell/log.sh
@@ -3,12 +3,24 @@ if test -z $1
     then
         echo 'log.sh <tag>'
     else
-        echo 'fix:'
-        git log $1..HEAD --pretty=format:"- %s" --grep fix | sed  's/fix//g'
-        echo '\n'
+        FIX=`git log $1..HEAD --pretty=format:"- %s" --grep fix | sed  's/fix//g'`
         
-        echo 'feature:'
-        git log $1..HEAD --pretty=format:"- %s" --grep feature | sed  's/feature//g'
+        if test -n FIX
+        then
+            echo 'fix:'
+            git log $1..HEAD --pretty=format:"- %s" --grep feature | sed  's/feature//g'
+        fi
         
-        echo '\n\n'
+        FEATURE=$(git log $1..HEAD --pretty=format:"- %s" --grep feature | sed  's/feature//g')
+        
+        if test -n FEATURE
+        then
+            echo 'feature:'
+            git log $1..HEAD --pretty=format:"- %s" --grep feature | sed  's/feature//g'
+        fi
+        
+        if test -n FIX || test -z FEATURE
+        then
+            echo '\n'
+        fi
 fi
