cd296f84bec7af6922043bb0d97591f9e5dfa063
coderaiser <mnemonic.enemy@gmail.com>
2014-09-16 04:51:22 -0400

    feature(config) add

--- cloudcmd.js
+++ cloudcmd.js
@@ -5,84 +5,32 @@
         DIR_LIB     = DIR       + 'lib/',
         DIR_SERVER  = DIR       + 'lib/server/',
         
-        fs          = require('fs'),
-        
         win         = require(DIR_SERVER + 'win'),
-        main        = require(DIR_SERVER + 'main'),
         update      = require(DIR_SERVER + 'update'),
-        tryRequire  = require(DIR_SERVER + 'tryRequire'),
         
-        Util        = require(DIR_LIB + 'util'),
         server      = require(DIR_LIB + 'server'),
         
-        Config      = main.config;
+        config      = require(DIR_SERVER + 'config');
         
     module.exports = function(params) {
-        readConfig(function(error, config) {
-            var keys;
+        var keys;
+        
+        if (params) {
+            keys = Object.keys(params);
             
-            if (error)
-                Util.log(error.message);
-            
-            if (!config)
-                config = {};
-            
-            if (params) {
-                keys = Object.keys(params);
-                
-                keys.forEach(function(item) {
-                    config[item] = params[item];
-                });
-            }
-            
-            init(config);
-        });
+            keys.forEach(function(name) {
+                config(name, params[name]);
+            });
+        }
         
         win.prepareCodePage();
-    };
-    
-    function init(config) {
-        Util.log('server dir: ' + DIR);
         
         if (update)
             update.get();
         
-        if (config) {
-            main.config = Config = config;
-            
-            if (config.test)
-                Config.server  = false;
-        }
-        
-        if (Config.logs) {
-            Util.log('log param setted up in config.json\n' +
-                'from now all logs will be writed to log.txt');
-            
-            writeLogsToFile();
-        }
+        if (config('test'))
+            config(server, false);
         
         server();
-    }
-    
-    function readConfig(callback) {
-        var error,
-            configPath  = DIR + 'json/config.json',
-            config      = tryRequire(configPath, function(err) {
-                if (err)
-                    error = err;
-            });
-        
-        Util.checkArgs(arguments, ['callback']);
-        
-        callback(error, config);
-    }
-    
-    /* function sets stdout to file log.txt */
-    function writeLogsToFile() {
-        var stdout      = process.stdout,
-            writeFile   = fs.createWriteStream('log.txt'),
-            write       = writeFile.write.bind(writeFile);
-        
-        stdout.write    = write;
-    }
+    };
 })();
