4bd43154c727e803c4939afd9abe051ce67eb51c
coderaiser <mnemonic.enemy@gmail.com>
2014-12-26 11:20:35 -0500

    feature(changelog) add prepend

--- gulp/tasks/changelog.js
+++ gulp/tasks/changelog.js
@@ -2,14 +2,21 @@
     'use strict';
     
     var changelog   = require('changelog-io'),
+        prepend     = require('prepend'),
         cl          = require('../cl');
     
     module.exports = function(callback) {
         cl(function(error, versionNew) {
-            if (error)
-                callback(error);
-            else
-                changelog(versionNew, callback);
+            var e = function(error) {
+                if (error)
+                    callback(error);
+                
+                return error;
+            };
+            
+            e(error) || changelog(versionNew, function(error, data) {
+                e(error) || prepend('ChangeLog', data, callback);
+            });
         });
     };
 })();
