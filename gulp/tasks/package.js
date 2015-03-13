851b802bbbcdc529b52a79b88534409d7208cea5
coderaiser <mnemonic.enemy@gmail.com>
2015-03-13 05:18:45 -0400

    refactor(gulp) package

--- gulp/tasks/package.js
+++ gulp/tasks/package.js
@@ -6,19 +6,20 @@
         
     module.exports = function(callback) {
         cl(function(error, versionNew) {
-            version(versionNew, function(error) {
-                if (error)
-                    callback(error);
-                else
-                    callback(null, 'package: done');
-            });
+            var fn = function(data, error) {
+                    var msg;
+                    
+                    if (!error)
+                        msg = data + ': done';
+                    
+                    callback(error, msg);
+                },
+                
+                fnPackage   = fn.bind(null, 'package'),
+                fnBower     = fn.bind(null, 'bower');
             
-            version(versionNew, {name: 'bower'}, function(error) {
-                if (error)
-                    callback(error);
-                else
-                    callback(null, 'bower: done');
-            });
+            version(versionNew, {name: 'package'}, fnPackage);
+            version(versionNew, {name: 'bower'}, fnBower);
         });
     };
 })();
