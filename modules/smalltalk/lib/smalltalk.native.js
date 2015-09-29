54a12b724e985be765a023657b402533b3429b82
coderaiser <mnemonic.enemy@gmail.com>
2015-09-29 07:04:56 -0400

    feature(bower) smalltalk v1.5.1

--- modules/smalltalk/lib/smalltalk.native.js
+++ modules/smalltalk/lib/smalltalk.native.js
@@ -1,54 +1,44 @@
-(function(global) {
+'use strict';
+
+(function (global) {
     'use strict';
-    
-    if (typeof module !== 'undefined' && module.exports)
-        module.exports = Smalltalk();
-    else
-        global.smalltalk = Smalltalk();
-        
-    
+
+    if (typeof module !== 'undefined' && module.exports) module.exports = Smalltalk();else global.smalltalk = Smalltalk();
+
     function Smalltalk() {
-        if (!(this instanceof Smalltalk))
-            return new Smalltalk();
-        
-        this.alert          = function(title, message) {
-            var promise = new Promise(function(resolve) {
+        if (!(this instanceof Smalltalk)) return new Smalltalk();
+
+        this.alert = function (title, message) {
+            var promise = new Promise(function (resolve) {
                 alert(message);
                 resolve();
             });
-            
+
             return promise;
         };
-        
-        this.prompt         = function(title, message, value, options) {
-            var o       = options, 
-                promise = new Promise(function(resolve, reject) {
-                    var noCancel = o && !o.cancel,
-                        result  = prompt(message, value);
-                    
-                    if (result !== null)
-                        resolve(result);
-                    else if (!noCancel)
-                        reject();
-                });
-            
+
+        this.prompt = function (title, message, value, options) {
+            var o = options,
+                promise = new Promise(function (resolve, reject) {
+                var noCancel = o && !o.cancel,
+                    result = prompt(message, value);
+
+                if (result !== null) resolve(result);else if (!noCancel) reject();
+            });
+
             return promise;
         };
-        
-        this.confirm         = function(title, message, options) {
-            var o           = options,
-                noCancel    = o && !o.noCancel,
-                promise     = new Promise(function(resolve, reject) {
-                    var is = confirm(message);
-                    
-                    if (is)
-                        resolve();
-                    else if (!noCancel)
-                        reject();
-                });
-            
+
+        this.confirm = function (title, message, options) {
+            var o = options,
+                noCancel = o && !o.noCancel,
+                promise = new Promise(function (resolve, reject) {
+                var is = confirm(message);
+
+                if (is) resolve();else if (!noCancel) reject();
+            });
+
             return promise;
         };
     }
-    
-})(this);
+})(typeof window !== 'undefined' && window);
---
+++ modules/smalltalk/src/smalltalk.native.js
@@ -0,0 +1,54 @@
+(function(global) {
+    'use strict';
+    
+    if (typeof module !== 'undefined' && module.exports)
+        module.exports = Smalltalk();
+    else
+        global.smalltalk = Smalltalk();
+        
+    
+    function Smalltalk() {
+        if (!(this instanceof Smalltalk))
+            return new Smalltalk();
+        
+        this.alert          = (title, message) => {
+            let promise = new Promise(function(resolve) {
+                alert(message);
+                resolve();
+            });
+            
+            return promise;
+        };
+        
+        this.prompt         = (title, message, value, options) => {
+            let o       = options, 
+                promise = new Promise(function(resolve, reject) {
+                    let noCancel = o && !o.cancel,
+                        result  = prompt(message, value);
+                    
+                    if (result !== null)
+                        resolve(result);
+                    else if (!noCancel)
+                        reject();
+                });
+            
+            return promise;
+        };
+        
+        this.confirm         = (title, message, options) => {
+            let o           = options,
+                noCancel    = o && !o.noCancel,
+                promise     = new Promise(function(resolve, reject) {
+                    let is = confirm(message);
+                    
+                    if (is)
+                        resolve();
+                    else if (!noCancel)
+                        reject();
+                });
+            
+            return promise;
+        };
+    }
+    
+})(typeof window !== 'undefined' && window);
