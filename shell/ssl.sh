2229be3ca0fbd3386c14f675ac5ff6b6352973ec
coderaiser <mnemonic.enemy@gmail.com>
2016-06-14 20:56:26 +0300

    chore(ssl) rm " "

--- shell/ssl.sh
+++ shell/ssl.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-openssl genrsa -out privatekey.pem 2048 
-openssl req -new -key privatekey.pem -out certrequest.csr 
+openssl genrsa -out privatekey.pem 2048
+openssl req -new -key privatekey.pem -out certrequest.csr
 openssl x509 -req -in certrequest.csr -signkey privatekey.pem -out certificate.pem
+
