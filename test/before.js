91c6396dacc2e43d28b28c6784c6cf2ad11c8443
coderaiser <coderaiser@cloudcmd.io>
2016-11-09 12:19:52 +0200

    test(before) prevent config overwriting

--- test/before.js
+++ test/before.js
@@ -1,14 +1,25 @@
 'use strict';
 
 const http = require('http');
+const fs = require('fs');
+const os = require('os');
 
 const express = require('express');
+const writejson = require('writejson');
+const readjson = require('readjson');
+
 const cloudcmd = require('..');
 
-module.exports = (fn) => {
+const pathConfig = os.homedir() + '/.cloudcmd.json';
+const currentConfig = readjson.sync.try(pathConfig);
+
+module.exports = (config, fn = config) => {
     const app = express();
     const server = http.createServer(app);
     const after = () => {
+        if (currentConfig)
+            writejson.sync(pathConfig, currentConfig);
+        
         server.close();
     };
     
