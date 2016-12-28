f1af8a76e53162f06330a517ce757dfac575c4d2
coderaiser <coderaiser@cloudcmd.io>
2016-12-28 13:39:26 +0200

    test(util) es2015-ify

--- test/server/util.js
+++ test/server/util.js
@@ -1,27 +1,24 @@
-(function() {
-    'use strict';
+'use strict';
+
+const test = require('tape');
+const DIR = '../../';
+const Util = require(DIR + 'common/util');
+
+test('getExt: no extension', function(t) {
+    const EXT = '';
+    const name = 'file-withot-extension';
+    const ext = Util.getExt(name);
     
-    var test    = require('tape'),
-        DIR     = '../../',
-        Util    = require(DIR + 'common/util');
+    t.equal(ext, EXT, 'should return "" when extension is none');
+    t.end();
+});
+
+test('getExt: return extension', function(t) {
+    const EXT = '.png';
+    const name = 'picture.png';
+    const ext = Util.getExt(name);
     
-    
-    test('getExt: no extension', function(t) {
-        var EXT     = '',
-            name    = 'file-withot-extension',
-            ext     = Util.getExt(name);
-        
-        t.equal(ext, EXT, 'should return "" when extension is none');
-        t.end();
-    });
-    
-    test('getExt: return extension', function(t) {
-        var EXT     = '.png',
-            name    = 'picture.png',
-            ext     = Util.getExt(name);
-        
-        t.equal(ext, EXT, 'should return ".png" in files "picture.png"');
-        t.end();
-    });
-    
-})();
+    t.equal(ext, EXT, 'should return ".png" in files "picture.png"');
+    t.end();
+});
+
