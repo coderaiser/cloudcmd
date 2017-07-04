fcd327ce7dde270d96a98339c3b75484a9463756
coderaiser <coderaiser@cloudcmd.io>
2017-07-04 16:35:16 +0300

    feature(package) edward v5.0.0

--- server/cloudcmd.js
+++ server/cloudcmd.js
@@ -211,7 +211,6 @@ function cloudcmd(prefix, plugins, modules) {
         
         edward({
             prefix  : prefix + '/edward',
-            minify,
             online,
             diff,
             zip,
