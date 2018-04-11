567a14f93dee8fe8e80c8484e960f4feac2a135b
coderaiser <coderaiser@cloudcmd.io>
2018-04-11 11:28:25 +0300

    feature(webpack) deepmerge -> webpack-merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,14 @@
 'use strict';
 
-const deepmerge = require('deepmerge');
-
-const isSimpleObject = ({constructor}) => constructor === Object;
-const {isArray} = Array;
-const isMergeableObject = (a) => isArray(a) || isSimpleObject(a);
+const merge = require('webpack-merge');
 
 const htmlConfig = require('./.webpack/html');
 const cssConfig = require('./.webpack/css');
 const jsConfig = require('./.webpack/js');
 
-module.exports = deepmerge.all([
+module.exports = merge([
     jsConfig,
     htmlConfig,
     cssConfig,
-], {isMergeableObject});
+]);
 
