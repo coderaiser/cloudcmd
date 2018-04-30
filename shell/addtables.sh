20afb1c1c9a88b4408bc5d8588e7cc1652a7c423
coderaiser <coderaiser@cloudcmd.io>
2018-04-30 12:09:11 +0300

    chore(shell) rm redundant

--- shell/addtables.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo iptables -t nat -L # look rules before
-sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8000
-sudo iptables -t nat -L # look reles after
-
-#sudo iptables -t nat -D PREROUTING 1
-#sudo iptables -t nat -D PREROUTING 2
--- shell/ssl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-openssl genrsa -out privatekey.pem 2048
-openssl req -new -key privatekey.pem -out certrequest.csr
-openssl x509 -req -in certrequest.csr -signkey privatekey.pem -out certificate.pem
-
