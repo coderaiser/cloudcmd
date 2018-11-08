bb9dde869e225a9321cdf94825cb600da5ae9617
coderaiser <coderaiser@cloudcmd.io>
2018-11-08 19:13:10 +0200

    test(fs) serve-once

--- test/rest/fs.js
+++ test/rest/fs.js
@@ -1,29 +1,16 @@
 'use strict';
 
-const test = require('tape');
-const {promisify} = require('es6-promisify');
-const pullout = require('pullout');
-const request = require('request');
-const before = require('../before');
+const tryToTape = require('try-to-tape');
+const test = tryToTape(require('tape'));
 
-const warp = (fn, ...a) => (...b) => fn(...b, ...a);
-const _pullout = promisify(pullout);
+const cloudcmd = require('../..');
+const {request} = require('serve-once')(cloudcmd);
 
-const get = promisify((url, fn) => {
-    fn(null, request(url));
-});
-
-test('cloudcmd: rest: fs: path', (t) => {
-    before((port, after) => {
-        get(`http://localhost:${port}/api/v1/fs`)
-            .then(warp(_pullout, 'string'))
-            .then(JSON.parse)
-            .then((dir) => {
-                t.equal('/', dir.path, 'should dir path be "/"');
-                t.end();
-                after();
-            })
-            .catch(console.error);
-    });
+test('cloudcmd: rest: fs: path', async (t) => {
+    const {body} = await request.get(`/api/v1/fs`);
+    const {path} = JSON.parse(body);
+    
+    t.equal('/', path, 'should dir path be "/"');
+    t.end();
 });
 
