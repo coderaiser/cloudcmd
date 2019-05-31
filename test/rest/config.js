2abe7d688d26be2db52a08df7011834ea56ff632
coderaiser <coderaiser@cloudcmd.io>
2019-05-31 18:30:16 +0300

    test(config) coverage

--- test/rest/config.js
+++ test/rest/config.js
@@ -1,23 +1,16 @@
 'use strict';
 
-const path = require('path');
-const os = require('os');
-
 const test = require('supertape');
-const readjson = require('readjson');
-const writejson = require('writejson');
 
 const cloudcmd = require('../..');
+const configManager = cloudcmd.createConfigManager();
+
 const {request} = require('serve-once')(cloudcmd, {
     config: {
         auth: false,
     },
-    configManager: cloudcmd.createConfigManager(),
+    configManager,
 });
-
-const manageConfig = require('../../server/config');
-
-const pathConfig = path.join(os.homedir(), '.cloudcmd.json');
 
 test('cloudcmd: rest: config: get', async (t) => {
     const {body} = await request.get('/api/v1/config', {
@@ -93,7 +86,7 @@ test('cloudcmd: rest: config: patch: no configDialog: statusCode', async (t) => 
         options,
     });
     
-    manageConfig('configDialog', true);
+    configManager('configDialog', true);
     
     t.equal(response.status, 404);
     t.end();
@@ -104,18 +97,11 @@ test('cloudcmd: rest: config: patch: save config', async (t) => {
         editor: 'dword',
     };
     
-    const originalConfig = readjson.sync.try(pathConfig);
-    
     await request.patch(`/api/v1/config`, {
         body,
     });
     
-    const config = readjson.sync(pathConfig);
-    
-    t.equal(config.editor, 'dword', 'should change config file on patch');
+    t.equal(configManager('editor'), 'dword', 'should change config file on patch');
     t.end();
-    
-    if (originalConfig)
-        writejson.sync(pathConfig, originalConfig);
 });
 
