7603fe2bdd7aec87c009b805c51d55c680cdb6f3
coderaiser <coderaiser@cloudcmd.io>
2021-01-18 14:08:14 +0200

    fix(client) dom: goToDirectory

---
+++ client/dom/index.spec.js
@@ -0,0 +1,30 @@
+'use strict';
+
+require('css-modules-require-hook');
+
+const {test, stub} = require('supertape');
+const mockRequire = require('mock-require');
+const {reRequire} = mockRequire;
+
+global.CloudCmd = {};
+
+test('cloudcmd: client: dom: goToDirectory', async (t) => {
+    const path = '';
+    const {CloudCmd} = global;
+    const loadDir = stub();
+    const prompt = stub().returns([null, path]);
+    
+    CloudCmd.loadDir = loadDir;
+    
+    mockRequire('./dialog', {
+        prompt,
+    });
+    
+    const {goToDirectory} = reRequire('.');
+    
+    await goToDirectory();
+    
+    t.calledWith(loadDir, [{path}]);
+    t.end();
+});
+
