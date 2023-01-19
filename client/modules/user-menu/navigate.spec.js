8abced61eac316987b5784016d44cca28e0fbc6d
coderaiser <coderaiser@cloudcmd.io>
2023-01-19 22:48:46 +0200

    feature: client: user-menu: navigate: add support of \D + [JK] to speed up vim navigation

--- client/modules/user-menu/navigate.spec.js
+++ client/modules/user-menu/navigate.spec.js
@@ -15,11 +15,11 @@ test('cloudcmd: user-menu: navigate: DOWN', (t) => {
         length: 3,
         selectedIndex: 0,
     };
-    
+
     navigate(el, {
         keyCode: DOWN,
     });
-    
+
     t.equal(el.selectedIndex, 1);
     t.end();
 });
@@ -29,11 +29,11 @@ test('cloudcmd: user-menu: navigate: J', (t) => {
         length: 3,
         selectedIndex: 0,
     };
-    
+
     navigate(el, {
         keyCode: J,
     });
-    
+
     t.equal(el.selectedIndex, 1);
     t.end();
 });
@@ -43,11 +43,11 @@ test('cloudcmd: user-menu: navigate: DOWN: bottom', (t) => {
         length: 3,
         selectedIndex: 2,
     };
-    
+
     navigate(el, {
         keyCode: DOWN,
     });
-    
+
     t.equal(el.selectedIndex, 0);
     t.end();
 });
@@ -57,11 +57,11 @@ test('cloudcmd: user-menu: navigate: K', (t) => {
         length: 3,
         selectedIndex: 2,
     };
-    
+
     navigate(el, {
         keyCode: K,
     });
-    
+
     t.equal(el.selectedIndex, 1);
     t.end();
 });
@@ -71,11 +71,11 @@ test('cloudcmd: user-menu: navigate: UP', (t) => {
         length: 3,
         selectedIndex: 2,
     };
-    
+
     navigate(el, {
         keyCode: UP,
     });
-    
+
     t.equal(el.selectedIndex, 1);
     t.end();
 });
@@ -85,11 +85,11 @@ test('cloudcmd: user-menu: navigate: UP: top', (t) => {
         length: 3,
         selectedIndex: 0,
     };
-    
+
     navigate(el, {
         keyCode: UP,
     });
-    
+
     t.equal(el.selectedIndex, 2);
     t.end();
 });
@@ -99,12 +99,68 @@ test('cloudcmd: user-menu: navigate', (t) => {
         length: 3,
         selectedIndex: 0,
     };
-    
+
     navigate(el, {
         keyCode: 0,
     });
-    
+
     t.equal(el.selectedIndex, 0, 'should not change');
     t.end();
 });
 
+test('cloudcmd: user-menu: navigate: DOWN: count', (t) => {
+    const el = {
+        length: 3,
+        selectedIndex: 0,
+    };
+
+    navigate(el, {
+        keyCode: 53,
+        key: '5',
+    });
+
+    navigate(el, {
+        keyCode: DOWN,
+    });
+
+    t.equal(el.selectedIndex, 5);
+    t.end();
+});
+
+test('cloudcmd: user-menu: navigate: J: count: to big', (t) => {
+    const el = {
+        length: 3,
+        selectedIndex: -Infinity,
+    };
+
+    navigate(el, {
+        keyCode: 53,
+        key: '5',
+    });
+
+    navigate(el, {
+        keyCode: J,
+    });
+
+    t.equal(el.selectedIndex, 2);
+    t.end();
+});
+
+test('cloudcmd: user-menu: navigate: K: count: to small', (t) => {
+    const el = {
+        length: 3,
+        selectedIndex: -Infinity,
+    };
+
+    navigate(el, {
+        keyCode: 53,
+        key: '5',
+    });
+
+    navigate(el, {
+        keyCode: K,
+    });
+
+    t.equal(el.selectedIndex, 0);
+    t.end();
+});
