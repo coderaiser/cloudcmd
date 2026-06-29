c04d363c4670373501c2d25b05d940b0126246d8
coderiaser <coderaiser@cloudcmd.io>
2026-06-29 18:16:34 +0300

    test: e2e: spawn

--- test-e2e/ratelimit.js
+++ test-e2e/ratelimit.js
@@ -1,15 +1,10 @@
 import process from 'node:process';
+import {spawn} from 'node:child_process';
 import {test} from 'supertape';
-
-let i = 0;
 
 test('cloudcmd: server: ratelimit: x-forwarded-for', async (t) => {
     const PORT = 3000;
-    
-    process.env.PORT = PORT;
-    process.env.CLOUDCMD_LOG = 0;
-    
-    await import('../bin/cloudcmd.js');
+    const child = await run(PORT);
     
     const {status} = await fetch(`http://localhost:${PORT}`, {
         headers: {
@@ -17,7 +12,7 @@ test('cloudcmd: server: ratelimit: x-forwarded-for', async (t) => {
         },
     });
     
-    process.kill(process.pid, 'SIGUSR1');
+    child.kill();
     
     t.notEqual(status, 500);
     t.end();
@@ -27,18 +22,34 @@ test('cloudcmd: server: ratelimit', async (t) => {
     const PORT = 3001;
     const STATUS = 429;
     
-    process.env.PORT = PORT;
-    process.env.CLOUDCMD_LOG = 0;
-    
-    await import(`../bin/cloudcmd.js?${i++}`);
+    const child = await run(PORT);
     
     for (let i = 0; i < 1000; i++) {
         await fetch(`http://localhost:${PORT}`);
     }
     
     const {status} = await fetch(`http://localhost:${PORT}`);
-    process.kill(process.pid, 'SIGUSR1');
+    child.kill();
     
     t.equal(status, STATUS);
     t.end();
 });
+
+function run(port) {
+    return new Promise((resolve, reject) => {
+        const child = spawn(new URL('../bin/cloudcmd.js', import.meta.url).pathname, [], {
+            env: {
+                ...process.env,
+                PORT: port,
+            },
+            stdio: ['ignore', 'pipe', 'pipe'],
+        });
+        
+        child.stdout.on('data', (a) => {
+            if (a.toString().includes('url'))
+                resolve(child);
+        });
+        
+        child.on('error', reject);
+    });
+}
