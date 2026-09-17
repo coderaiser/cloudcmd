97ffff3beb60a19c692bfd6ab7107009c1c2c68c
Yabi23 <yabi@tlen.pl>
2026-09-17 22:04:33 +0200

    test: remove explanatory comments from i18n e2e spec for code cleanliness

--- test-e2e/client/i18n.js
+++ test-e2e/client/i18n.js
@@ -2,10 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Cloud Commander i18n Client E2E Tests', () => {
     test('should inject __CLOUDCMD_I18N_PACK__ into index.html during bootstrap', async ({ page }) => {
-        // 1. Przejście na stronę główną (adres pobierany automatycznie z baseURL Playwrighta)
         await page.goto('/');
         
-        // 2. Weryfikacja czy obiekt i18n istnieje w oknie przeglądarki
         const i18nPack = await page.evaluate(() => window.__CLOUDCMD_I18N_PACK__);
         
         expect(i18nPack).toBeDefined();
