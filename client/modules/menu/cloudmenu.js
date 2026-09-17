21cdb18f826590dc332cf33acdd8869fa7379ae3
coderaiser <coderaiser@cloudcmd.io>
2026-09-17 21:29:45 +0000

    feature: menu: static import aleman/menu/bundle via rspack

--- client/modules/menu/cloudmenu.js
+++ client/modules/menu/cloudmenu.js
@@ -1,29 +1,17 @@
 import {supermenu} from 'supermenu';
+import {createMenu as createAlemanMenu} from 'aleman/menu/bundle';
 
 const noop = () => {};
 const {CloudCmd} = globalThis;
 
 export const createCloudMenu = async (fm, options, menuData) => {
-    const createMenu = await loadMenu();
+    const createMenu = CloudCmd.config('menu') === 'aleman' ? createAlemanMenu : createSupermenu;
     const menu = await createMenu(fm, options, menuData);
     
     menu.addContextMenuListener = menu.addContextMenuListener || noop;
     
     return menu;
 };
-
-async function loadMenu() {
-    if (CloudCmd.config('menu') === 'aleman') {
-        const {prefix} = CloudCmd;
-        const {host, protocol} = globalThis.location;
-        const url = `${protocol}//${host}${prefix}/node_modules/aleman/menu/menu.js`;
-        const {createMenu} = await import(/* webpackIgnore: true */url);
-        
-        return createMenu;
-    }
-    
-    return createSupermenu;
-}
 
 function createSupermenu(name, options, menuData) {
     const element = document.querySelector('[data-name="js-fm"]');
