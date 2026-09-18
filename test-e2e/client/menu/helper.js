b4779a2b1f9fc93e6fffc68af38461626334161d
coderaiser <mnemonic.enemy@gmail.com>
2026-09-18 14:16:30 +0000

    test: e2e: menu: add shared helper

---
+++ test-e2e/client/menu/helper.js
@@ -0,0 +1,39 @@
+const goto = (page) => page.goto('/', {
+    waitUntil: 'networkidle',
+});
+const waitForPanel = (page) => page
+    .locator('.files li')
+    .first()
+    .waitFor();
+
+export const createMenuHelper = (page) => ({
+    async open() {
+        await goto(page);
+        await waitForPanel(page);
+        await page.locator('[data-name="js-left"]').click({
+            button: 'right',
+        });
+    },
+    async pressKey(key) {
+        await page.keyboard.press(key);
+    },
+    async waitForMenu() {
+        await page
+            .locator('ul.menu:not(.menu-hidden)')
+            .first()
+            .waitFor();
+    },
+    isVisible: () => page
+        .locator('ul.menu:not(.menu-hidden)')
+        .first()
+        .isVisible(),
+    isHidden: () => page
+        .locator('ul.menu')
+        .first()
+        .isHidden(),
+    selectedText: () => page
+        .locator('.menu-item-selected label')
+        .first()
+        .textContent(),
+    itemTexts: () => page.locator('.menu > .menu-item > label').allTextContents(),
+});
